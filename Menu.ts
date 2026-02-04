95ad29f895c210eac61952ac2c0186ed7f147791
Sofia Sabrina Silva <sofiasabrinasilva@gmail.com>
2026-02-04 17:30:20 -0300

    :sparkle: Adição de funcionalidades

--- Menu.ts
+++ Menu.ts
@@ -1,8 +1,18 @@
 import leia from "readline-sync"
 import { colors } from "./src/util/Colors"
+import { Conta } from "./src/model/Conta"
 
 export function main() {
     let opcao: number
+
+    const c1 = new Conta(1, 1234, "Sofia", 1, 1000000.00);
+    c1.visualizar();
+
+    console.log("Sacar RS 100.00: ", c1.sacar(100.00));
+    console.log("Sacar RS 200.000.00: ", c1.sacar(200000.00));
+    console.log("Sacar RS 0.0", c1.sacar(0.0) )
+
+
 
     while (true) {
         console.log(colors.bg.black, colors.fg.magentastrong, `
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,3 +1,164 @@
+import { colors } from "../util/Colors";
+
 export class Conta {
 
+    private _numero: number;
+    private _agencia: number;
+    private _titular: string;
+    private _tipo: number;
+    private _saldo: number;
+
+
+	constructor(numero: number, agencia: number, titular: string, tipo: number, saldo: number) {
+		this._numero = numero;
+		this._agencia = agencia;
+		this._titular = titular;
+		this._tipo = tipo;
+		this._saldo = saldo;
+	}
+
+    /**
+     * Getter numero
+     * @return {number}
+     */
+	public get numero(): number {
+		return this._numero;
+	}
+
+    /**
+     * Getter agencia
+     * @return {number}
+     */
+	public get agencia(): number {
+		return this._agencia;
+	}
+
+    /**
+     * Getter titular
+     * @return {string}
+     */
+	public get titular(): string {
+		return this._titular;
+	}
+
+    /**
+     * Getter tipo
+     * @return {number}
+     */
+	public get tipo(): number {
+		return this._tipo;
+	}
+
+    /**
+     * Getter saldo
+     * @return {number}
+     */
+	public get saldo(): number {
+		return this._saldo;
+	}
+
+    /**
+     * Setter numero
+     * @param {number} value
+     */
+	public set numero(value: number) {
+		this._numero = value;
+	}
+
+    /**
+     * Setter agencia
+     * @param {number} value
+     */
+	public set agencia(value: number) {
+		this._agencia = value;
+	}
+
+    /**
+     * Setter titular
+     * @param {string} value
+     */
+	public set titular(value: string) {
+		this._titular = value;
+	}
+
+    /**
+     * Setter tipo
+     * @param {number} value
+     */
+	public set tipo(value: number) {
+		this._tipo = value;
+	}
+
+    /**
+     * Setter saldo
+     * @param {number} value
+     */
+	public set saldo(value: number) {
+		this._saldo = value;
+	}
+
+
+     // Métodos Auxiliares
+
+     public sacar(valor: number): boolean{
+
+        if(valor <= 0){
+                console.log(colors.fg.red, "O valor deve ser positivo.", colors.reset);
+                return false;
+        }
+
+        if(valor > this._saldo){
+            console.log(colors.fg.red, "Saldo Insuficiente!", colors.reset);
+            return false;
+        }
+        
+        this._saldo -= valor;
+        return true;
+     }
+
+    public depositar(valor: number): void{
+        
+        if(valor <= 0)
+           console.log(colors.fg.red, "O valor deve ser positivo.", colors.reset);
+            
+        else {
+             this._saldo += valor;
+        }
+
+        this._saldo -= valor;
+           
+    }
+
+    public visualizar(): void{
+
+        let tipo: string
+
+        switch(this._tipo){
+
+            case 1:
+                tipo = "Conta Corrente";
+            break
+
+            case 2:
+                tipo = "Conta Poupança";
+            break
+
+            default:
+                tipo = "Tipo Inválido.";
+
+        }
+
+        console.log("\n***************************************");
+        console.log("             DADOS DA CONTA              ");
+        console.log("***************************************\n");
+        console.log(`Número da Conta: ${this._numero}`);
+        console.log(`Número da Agência: ${this._agencia}`);
+        console.log(`Nome do Titular: ${this._titular}`);
+        console.log(`Número da Conta: ${tipo}`);
+        console.log(`Saldo da Conta:  R$ ${this._saldo.toFixed(2)}`);
+    }
+
+
+    
 }
+
